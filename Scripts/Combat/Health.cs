6db4468a11dd8a6131ebb4ae1eadda3865526235
jeptun <73310547+jeptun@users.noreply.github.com>
2020-12-30 16:21:56 +0100

    Add files via upload
    RPG scrpt unity

---
+++ Scripts/Combat/Health.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class Health : MonoBehaviour
+    {
+       [SerializeField] float healthPoints = 100f;
+
+        bool isDead = false;
+
+        public void TakeDamage(float damage)
+        {
+            healthPoints = Mathf.Max(healthPoints - damage, 0);
+            if (healthPoints == 0)
+            {
+                Die();
+            }
+        }
+
+        private void Die()
+        {
+            if (isDead) return;
+
+            isDead = true;
+            GetComponent<Animator>().SetTrigger("die");
+        }
+    }
+
+}
